95a94d33c2417066c8b082fa3676f0fe0612b43a
Mich43ll <mosoriobarahona@gmail.com>
2022-11-06 14:44:02 -0600

    arreglo de promedio

--- view/notas/registrar.php
+++ view/notas/registrar.php
@@ -6,7 +6,7 @@ $obj->guardar(
 $_POST['primerParcial'],
 $_POST['segundoParcial'],
 $_POST['tercerParcial'],
-$_POST['promedio'],
+$_POST['promedio']=($_POST['primerParcial']+$_POST['segundoParcial']+$_POST['tercerParcial'])/3,
 $_POST['idClase'],
 $_POST['idAlumno'],
 $_POST['idEmpleado'],
