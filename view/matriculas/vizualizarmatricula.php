e688ccbd59061f78e19cc4095aa2436107c15ebc
Mich43ll <mosoriobarahona@gmail.com>
2022-11-08 11:49:52 -0600

    eliminacion de visualizar matricula.php

--- view/matriculas/vizualizarmatricula.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("C://wamp64/www/Escuela/controller/matriculascontroller.php");
-$obj = new matriculasController();
-$obj->guardar( 
-    $_POST['IdAlumno'],
-    $_POST['Jornada'],
-    $_POST['idGrado']
-);
-?>
